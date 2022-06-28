7f8adeb41d52d2921e799640f309363f2f88ddab
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-06-28 08:12:10 -0500

    Correct id from random to UUID

--- item.rb
+++ item.rb
@@ -1,9 +1,11 @@
+require 'UUID'
+
 class Item
   attr_accessor :publish_date, :archived
   attr_reader :label, :genre, :author, :source
 
   def initialize(publish_date, archived: false)
-    @id = Random.rand(1..100)
+    @id = UUID.new.generate
     @publish_date = publish_date
     @archived = archived
   end
