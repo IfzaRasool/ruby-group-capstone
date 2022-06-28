4a495d8e91efc4c4bebe9948724f0207b0327d6b
IfzaRasool <ifzarasoolarain@gmail.com>
2022-06-28 12:56:43 +0300

    Add author class

---
+++ classes/author.rb
@@ -0,0 +1,14 @@
+class Author
+  attr_accessor :first_name, :last_name, :items
+
+  def initialize(first_name, last_name, items)
+    @id = Random.rand(1..100)
+    @first_name = first_name
+    @last_name = last_name
+    @items = items
+  end
+
+  def add_item
+    # method add item
+  end
+end
