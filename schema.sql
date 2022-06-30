b8a93cfb499cd045243c2a40d550819c72b7b9c1
IfzaRasool <ifzarasoolarain@gmail.com>
2022-06-30 12:45:50 +0300

    Add schema for music album and genre table

--- schema.sql
+++ schema.sql
@@ -2,5 +2,5 @@
 CREATE DATABASE catalog_of_things
 -- Create Music Album Table
 CREATE TABLE music_albums(id serial not null primary key,archived boolean,publish_date date, on_spotify boolean);
--- created genres table
+-- create genres table
 CREATE TABLE  genres(id serial not null primary key,name varchar(255));
