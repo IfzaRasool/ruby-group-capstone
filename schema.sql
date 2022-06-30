f08ad988b234096eef9ba2c1cb61bdf7b64b195f
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-06-30 04:53:01 -0500

    Add tables to schema
    -Create movies table
    -Create sources table

--- schema.sql
+++ schema.sql
@@ -4,3 +4,11 @@ CREATE DATABASE catalog_of_things
 CREATE TABLE music_albums(id serial not null primary key,archived boolean,publish_date date, on_spotify boolean);
 -- create genres table
 CREATE TABLE  genres(id serial not null primary key,name varchar(255));
+
+
+
+-- create movies table
+CREATE TABLE movies(id INT PRIMARY KEY, silent BOOLEAN, FOREIGN KEY(id) REFERENCES item(id));
+
+-- create sources table
+CREATE TABLE sources(id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(255) PRIMARY KEY(id));
