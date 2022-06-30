4527c25be1070046f8d55d9426103abfab26f98c
maxthestranger <thestranger@gmail.com>
2022-06-30 13:17:48 +0300

    add games and author sql

--- schema.sql
+++ schema.sql
@@ -1,14 +1,60 @@
 -- Create Database
-CREATE DATABASE catalog_of_things
--- Create Music Album Table
-CREATE TABLE music_albums(id serial not null primary key,archived boolean,publish_date date, on_spotify boolean);
--- create genres table
-CREATE TABLE  genres(id serial not null primary key,name varchar(255));
+CREATE DATABASE catalog_of_things;
+DROP IF EXISTS music_albums, genres, movies, sources, authors, games;
 
+-- Items
+CREATE TABLE items(
+    id VARCHAR(100) NOT NULL PRIMARY KEY,
+    genre_id VARCHAR(100),
+    author_id VARCHAR(100),
+    label_id VARCHAR(100),
+    source_id VARCHAR(100),
+    publish_date DATE NOT NULL,
+    archived BOOLEAN NOT NULL,
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (author_id) REFERENCES authors (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id),
+    FOREIGN KEY (source_id) REFERENCES sources (id),
+);
 
+-- Albums
+CREATE TABLE music_albums(
+    id VARCHAR(100) not null primary key,
+    archived boolean,
+    publish_date date,
+    on_spotify boolean
+);
 
--- create movies table
-CREATE TABLE movies(id INT PRIMARY KEY, silent BOOLEAN, FOREIGN KEY(id) REFERENCES item(id));
+-- Movies
+CREATE TABLE movies(
+    id VARCHAR(100) NOT NULL PRIMARY KEY,
+    silent BOOLEAN
+);
 
--- create sources table
-CREATE TABLE sources(id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(255) PRIMARY KEY(id));
+-- Games
+CREATE TABLE games(
+    id VARCHAR(100) NOT NULL PRIMARY KEY,
+    multiplayer BOOLEAN,
+    last_played_at DATE,
+)
+
+-- Sources
+CREATE TABLE sources(
+    id VARCHAR(100) NOT NULL,
+    name VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+-- Genres
+CREATE TABLE genres(
+    id VARCHAR(100) not null primary key,
+    name varchar(255)
+);
+
+-- Authors
+CREATE TABLE authors(
+    id VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    PRIMARY KEY(id)
+)
