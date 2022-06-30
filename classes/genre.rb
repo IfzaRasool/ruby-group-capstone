e7c225508d13ff5728566b831ee056f363c965ce
IfzaRasool <ifzarasoolarain@gmail.com>
2022-06-30 12:12:19 +0300

    Add unit test of  genre and musicalbum

--- classes/genre.rb
+++ classes/genre.rb
@@ -2,6 +2,7 @@ require 'UUID'
 
 class Genre
   attr_accessor :name
+  attr_reader :items
 
   def initialize(name)
     @id = UUID.new.generate
--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -2,14 +2,17 @@ require_relative '../classes/music_album'
 
 describe MusicAlbum do
   context 'Music Album ' do
-    it 'on the spotify return true' do
-      music_album = MusicAlbum.new('2002-12-12', true)
-
+    music_album = MusicAlbum.new('2002/12/12', true)
+    it 'on the spotify return true' do 
       expect(music_album.on_spotify).to equal(true)
     end
-    # it 'can be archived' do
-    #   music_album = MusicAlbum.new('15', true)
-    #   expect(music_album.can_be_archived?).to eq(true)
-    # end
+
+    it 'Published Date' do
+      expect(music_album.publish_date).to eq Date.parse('2002/12/12')
+    end
+
+    it 'can be archived' do
+      expect(music_album.archived).to eq(false)
+    end
   end
 end
